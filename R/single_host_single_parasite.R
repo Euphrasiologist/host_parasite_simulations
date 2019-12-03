ee1784b53c76262a591969ffe21730c143e7fc07
Max Brown <max.brown@ed.ac.uk>
2019-12-03 15:29:48 +0000

    Need the R subfolder it turns out...

--- R/single_host_single_parasite.R
+++ R/single_host_single_parasite.R
@@ -1,22 +1,16 @@
-#' Single host, single parasite simulation. 
+#' Single host, single parasite simulation. Take a host grid containing a number of hosts with some benefit to the parasite (randomly spaced, but fixed). Then overlay a parasite grid contaning some parasites (randomly spaced). If hosts and parasites co-occur, the parasite will reproduce. Cumulative offspring then randomly overlaid on grid.
 #' 
-#' Take a host grid containing a number of hosts with some benefit to the parasite (randomly spaced, but fixed). Then overlay a parasite grid contaning some parasites (randomly spaced). If hosts and parasites co-occur, the parasite will reproduce. Cumulative offspring then randomly overlaid on grid.
-#' 
-#' 
-#' @details Model assumes (1) that hosts are fixed in position and perennial, and are unaffected by parasitism. (2) no effect on parasite of nearby hosts, (3) parasites seed randomly over the field, (4) all parasites survive to end of generation, (5) parasites do not produce any offspring without a host, and all parasites with a host reproduce and (6) no host independent mortality of parasites
-#' @seealso onehost_oneparasite_dynamics to replicate many times.
+#' @details Model assumes 1 - that hosts are fixed in position and perennial, and are unaffected by parasitism. 2 - no effect on parasite of nearby hosts, 3 - parasites seed randomly over the field, 4 - all parasites survive to end of generation, 5 - parasites do not produce any offspring without a host, and all parasites with a host reproduce and 6 - no host independent mortality of parasites
 #' @param field.size size of host parasite matrix to play on. Must be square.
 #' @param s the selective advantage to a parasite of landing on a host.
 #' @param host.number number of hosts to seed the matrix with.
 #' @param parasite.number number of parasites to seed the matrix with.
 #' @param gens number of generations to run the model for.
-#' 
+#' @return a list of the parasite dynamics, spatial structure and the host matrix
 #' @export
 #' @examples
-#' 
 #' # should print out simulation from default parameters
 #' onehost_oneparasite() 
-
 
 onehost_oneparasite <- function(field.size = 5^2, s = 1, host.number = 5, parasite.number = 5, gens = 100){
   fz <- field.size
